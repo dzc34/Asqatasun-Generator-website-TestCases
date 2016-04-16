74b2f4f44d2a643d36682d5573dc5a79b293faea
Fabrice Gangler <fabrice@asqatasun.org>
2016-04-16 17:26:40 +0200

    add www-src/

--- Asqatasun-generator-TestCases-website.sh
+++ Asqatasun-generator-TestCases-website.sh
@@ -3,16 +3,18 @@
 ## WIP
 
 # 
-rm -rf ./www
-rm -rf ./tmp-build
+rm -rf ./www-build
+rm -rf ./build-tmp
 
 # 
-mkdir -p ./tmp-build
-cd ./tmp-build
+mkdir -p ./build-tmp
+cd ./build-tmp
 git clone https://github.com/Asqatasun/Asqatasun.git
 
 # 
 cd ..
-mv -v ./tmp-build/Asqatasun/rules/rules-rgaa3.0/src/test/resources/testcases/rgaa30 www
-rm -rf ./tmp-build
+mkdir -p www-build
+cp -vr www-src/* www-build/
+mv -v ./build-tmp/Asqatasun/rules/rules-rgaa3.0/src/test/resources/testcases/rgaa30 ./www-build/rgaa-3.0-test-cases/
+rm -rf ./build-tmp
 
